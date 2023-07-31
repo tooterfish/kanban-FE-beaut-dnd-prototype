ec819e974354483b22ccf59e8247654bb8db6ca0
Harry Fleming <email@harryfleming.net>
2023-07-31 16:21:04 +0100

    add edit button to list title

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 import Task from './Task'
 
+import editIcon from '../assets/edit-box-line.svg'
+
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
@@ -14,9 +16,14 @@ const Container = styled.div`
 
 const Title = styled.h3`
   margin: 0px;
-  margin-top: 4px;
   padding: 8px;
   border-bottom: 1px solid lightgrey;
+  &:hover {
+    background-color: whitesmoke;
+  }
+  &:hover .hidden {
+    display: block;
+  }
 `
 
 const TaskList = styled.div`
@@ -27,6 +34,27 @@ const TaskList = styled.div`
   min-height: 200px;
   background-color: ${props => (props.$isDraggingOver ? 'whitesmoke' : 'white')}
 `
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const Button = styled.button`
+  all: unset;
+  width: 20px;
+  height: 20px;
+  position: fixed;
+  transform: translate(0px, -22px);
+  opacity: 20%;
+  &:hover {
+    opacity: 100%;
+    background-color: whitesmoke;
+    cursor: pointer;
+  }
+  display: none;
+`
+
 
 export default function List({listId, taskOrder, index}) {
   const [list, setList] = useState(data.lists[listId])
@@ -39,7 +67,11 @@ export default function List({listId, taskOrder, index}) {
           $isDraggingOver={snapshot.isDraggingOver} 
           {...provided.draggableProps}
         >
-          <Title {...provided.dragHandleProps}>{list.title}</Title>
+          <Title {...provided.dragHandleProps}>{list.title}
+          <ButtonWrapper>
+            <Button className="hidden"><img src={editIcon} width='25' height='25'/></Button>
+          </ButtonWrapper>
+          </Title>
           <Droppable droppableId={listId} type='task'>
             {(provided, snapshot) => 
               <TaskList 
