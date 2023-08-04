7400597f001b398347893cc52fa2c3674e12300f
Harry Fleming <email@harryfleming.net>
2023-08-04 14:55:44 +0100

    changed data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 const data = {
   tasks: {
     'task-1': { id: 'task-1', content: 'A task'},
-    'task-2': { id: 'task-2', content: 'Another task'},
+    'task-2': { id: 'task-2', content: 'Task too electric boogaloo'},
     'task-3': { id: 'task-3', content: 'Is my work ever done?'},
-    'task-4': { id: 'task-4', content: 'Idle hands etc... type some rubbish here so we can test longer strings'},
+    'task-4': { id: 'task-4', content: 'Idle hands etc... put something here to test the styling still works for longer strings'},
   },
 
   lists: {
